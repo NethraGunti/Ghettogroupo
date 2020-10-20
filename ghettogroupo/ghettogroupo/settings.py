322caf2fdef9a9afaf500c26c8f3a065cc175a3c
Nethra16 <nethrag16@gmail.com>
2020-10-20 12:54:20 +0530

    integrated postgres and updated requirements

--- ghettogroupo/ghettogroupo/settings.py
+++ ghettogroupo/ghettogroupo/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'ghettogroupo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Ghetto',
+        'USER': 'ghetto',
+        'PASSWORD': 'ghetto',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
